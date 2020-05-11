61f58148fff605b5e9336b8f1e494153834b1179
dmdeveloper <kudatawira@gmail.com>
2020-05-11 19:05:29 +0200

    Update setImages.js

--- inst/htmlwidgets/lib/setImages.js
+++ inst/htmlwidgets/lib/setImages.js
@@ -1,18 +1,25 @@
 
 //console.log("setImages.js  panwidgts");
 function setCanvas(targetID,imgSrc){
-let imgId = 'currnt-img_' + targetID.substring(14, targetID.length);
-if ( $('#'+targetID+' img' ).length ) {
-     $('#'+targetID+' img' ).attr('src',imgSrc );
-    //console.log("img exist : ");
+  console.log('setCanvas');
+
+  let imgId = 'currnt-img_' + targetID.substring(14, targetID.length);
+
+  console.log('imgId ' + imgId)
+
+  if ( $('#'+targetID+' img' ).length ) 
+  {
+      $('#'+targetID+' img' ).attr('src',imgSrc );
+      //console.log("img exist : ");
   }else{
 
+      console.log('Image already exist');
 
-     $('#'+targetID).prepend($('<img>',{id:imgId,src:imgSrc,alt:'camtrap',width:'100%',height:'500px'}));
+      $('#'+targetID).prepend($('<img>',{id:imgId,src:imgSrc,alt:'camtrap',width:'100%',height:'500px'}));
 
 
-      wheelzoom(document.querySelector("#"+imgId));
-						//$("#"+imgId).imgViewer();
+        wheelzoom(document.querySelector("#"+imgId));
+              //$("#"+imgId).imgViewer();
 
-  }
+    }
 }
