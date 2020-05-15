a4c6096c86895112a9fc97b6ed83f0cc54d999ec
dmdeveloper <kudatawira@gmail.com>
2020-05-15 14:11:08 +0200

    Update sngl_img_viewer.js

--- inst/htmlwidgets/sngl_img_viewer.js
+++ inst/htmlwidgets/sngl_img_viewer.js
@@ -13,7 +13,7 @@ HTMLWidgets.widget({
       renderValue: function(x) {
 
         // TODO: code to render the widget, e.g.
-        console.log("sngl_img_viewer.js  15-05-20 11:30");
+        console.log("sngl_img_viewer.js  15-05-20 14:10");
         
         let spcs_idntfctns_scndry_img = new viewerClass(
                                             "spcs_idntfctn_id_rf_2",
