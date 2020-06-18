8598a9c9365277df60df11a5ec283bf39c8519d2
dmdeveloper <kudatawira@gmail.com>
2020-06-18 12:32:31 +0200

    Update p3_dshbrd_line_bar_chart.js

--- inst/htmlwidgets/p3_dshbrd_line_bar_chart.js
+++ inst/htmlwidgets/p3_dshbrd_line_bar_chart.js
@@ -10,6 +10,7 @@ HTMLWidgets.widget({
   },
 
     renderValue: function(el, x, instance) {
+     console.log('Target ID v2 : ' + el.id );
        var Data = x;
 
     if(typeof(instance.chart) != 'undefined'){
@@ -18,6 +19,7 @@ HTMLWidgets.widget({
       instance.Data = Data;
 
       var chart = c3.generate({
+
         bindto: '#' + el.id,
         title: {
           text: Data.title
@@ -49,11 +51,24 @@ HTMLWidgets.widget({
         transition: {
           duration: Data.transition
         },
-        size:
-        {height: Data.height}
+        size: {
+          height: Data.height
+
+        },
+        onrendered: function () {
+          console.log('Testing');
+          let prnt = document.getElementById("dshbrd_line_bar");
+          prnt.style.textAlign = "center";
+          let elChild =  document.createElement("h4");
+          elChild.innerText = "PantheraIDS Active Logins Line Bar Chart.";
+          prnt.insertBefore(elChild, prnt.firstChild);
+          console.log('Target ID v1 : ' + el.id );
+
+        },
       });
 
       instance.chart = chart;
+
     //}
 
     // Store chart instance in the div for later manipulation
@@ -69,4 +84,15 @@ HTMLWidgets.widget({
   }
 
 
+
 });
+
+/*alert('Testing');
+let prnt = document.getElementById("usg_dshbrd-lineBarChart");
+prnt.style.textAlign = "center";
+let elChild =  document.createElement("h4");                 // Create a <p> element
+elChild.innerText = "PantheraIDS Active Logins Line Bar Chart.";
+prnt.insertBefore(elChild, prnt.firstChild);*/
+// Insert text
+
+
