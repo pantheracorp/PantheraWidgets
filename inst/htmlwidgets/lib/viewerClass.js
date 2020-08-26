7b63bfedd8e50ab64ba3621dc9cad1c7efa323f3
dmdeveloper <kudatawira@gmail.com>
2020-08-26 15:23:14 +0200

    dynamic viewer tester

--- inst/htmlwidgets/lib/viewerClass.js
+++ inst/htmlwidgets/lib/viewerClass.js
@@ -1,188 +1,191 @@
 class viewerClass {
 
-      constructor(moduleId,csvfile)
-      {
-        this.csvfile = csvfile;
-        this.moduleId =moduleId;
-        this.currentIndex = 0;
-        this.result = [];
-        this.errorImg = '/srv/shiny-server/www/PantheraIDS_image_not_found_2.jpg';
-      }
-      // fetchServerData
+  constructor(moduleId, csvfile) {
+    this.csvfile = csvfile;
+    this.moduleId = moduleId;
+    this.currentIndex = 0;
+    this.result = [];
+    this.errorImg = '/srv/shiny-server/www/PantheraIDS_image_not_found_2.jpg';
+  }
+  // fetchServerData
 
-      fetchServerData(file)
-      {
-         getFile(file).then(data => this.readServerData(data));
-      }
+  fetchServerData(file) {
+    console.log("fetchServerData");
+    getFile(file).then(data => this.readServerData(data));
+  }
 
-      fetchServerDataTest(file)
-      {
-       
-         getFileTest(file).then(data => this.readServerData(data));
-      }
+  fetchServerDataTest(file) {
 
-      processImgSrc(arry){
-          //alert('processImgSrc : ' + this.moduleId);
-          //console.log('processImgSrc');
-          let tempArray = [];
-          arry.forEach(function(item){
-            let src  = ((item.trim()).replace(/['"]+/g, '')).replace(/(\r\n|\n|\r)/gm,"");
-              tempArray.push(src);
-          });
-          
-          return tempArray;
-      }
+    getFileTest(file).then(data => this.readServerData(data));
+  }
 
-      displayImage(){
+  processImgSrc(arry) {
+    //alert('processImgSrc : ' + this.moduleId);
+    //console.log('processImgSrc');
+    let tempArray = [];
+    arry.forEach(function (item) {
+      let src = ((item.trim()).replace(/['"]+/g, '')).replace(/(\r\n|\n|\r)/gm, "");
+      tempArray.push(src);
+    });
 
-        console.log('displayImage');
-        //console.log(this.result.length);
+    return tempArray;
+  }
 
-        if(this.imgexist(this.result[0]) == false){
-            //alert('Alert img not exist');
-            this.result[0] = this.errorImg;
-        }
-        if(this.moduleId === "spcs_idntfctn_id_rf_1"){
+  displayImage() {
 
-          setCanvas( this.moduleId,this.result[0]);
-        }
-        if(this.moduleId === "spcs_idntfctn_id_rf_2"){
-          setCanvas( this.moduleId,this.result[0]);
-        }
-        if(this.moduleId === 'pttrn_rcgntn_orgnl_imgs_1'){
-          //console.log('case pttrn_rcgntn_orgnl_imgs_1');
-          setCanvas( this.moduleId,this.result[0]);
-        }
-        if(this.moduleId === 'pttrn_rcgntn_orgnl_imgs_2'){
-          //console.log('case pttrn_rcgntn_orgnl_imgs_2');
-          setCanvas( this.moduleId,this.result[0]);
-        }
+    console.log('displayImage');
+    //console.log(this.result.length);
 
-        this.sendDataToShinny();
-       
-      }
+    /*if (this.imgexist(this.result[0]) == false) {
+      //alert('Alert img not exist');
+      this.result[0] = this.errorImg;
+    }*/
+    if (this.moduleId === "spcs_idntfctn_id_rf_1") {
 
-      restart(){
-        this.result.length = 0;
-        this.currentIndex = 0;
-      }
-
-
-      readServerData(response) {
-
-        let respArray = [];
-        if(response === null )
-        {
-          console.log(" Error in reading your images.Please check if all requirements are provided.");
-        }
-        else{
-          //respArray = response.split(',');
-              //respArray.splice(0, 1);
-          respArray = response.split("\n");
-          respArray.shift();
-          
-
-          if(respArray[respArray.length-1]==""){
-            //console.log('pop');
-            respArray.pop();
-          }
-
-          for(let i = 0 ; i < respArray.length; i++ ){
-            //respArray[i].replace(',','');
-            let src = respArray[i].substring(respArray[i].indexOf('/'),respArray[i].lastIndexOf('/'))+'/'+respArray[i].substring(0,respArray[i].indexOf('/'));
-            //src.replace(',','');
-            //console.log(src);
-            this.result.push(src.replace(',',''));
-           }
-          //console.log("respArray[0] : " + respArray[0]);
-          // respArray[0] = respArray[respArray.length - 1] + respArray[0];
-          // respArray.splice(respArray.length - 1, 1);
-          // this.result =  this.processImgSrc(respArray);
-          console.log(this.moduleId + 'Total Imgs : ' + (this.result.length));
-          //console.log(this.result)
-          this.displayImage();
-
-        }
-      }
-
-      reset(){
-         console.log('reset ' + this.moduleId + 'indx ' + this.currentIndex);
-         
-         this.currentIndex = 0;
-         console.log('img : ' + this.result[this.currentIndex]);
-         this.sendDataToShinny();
-         $('#'+this.moduleId+' img' ).attr('src', this.result[this.currentIndex] );
-
-      }
-
-      next() {
-         console.log('next : ' + this.moduleId + 'indx ' + this.currentIndex);
-         
-        
-        if(this.currentIndex == (this.result).length-1){
-        }
-        else{
-           if(this.imgexist(this.result[this.currentIndex+1]) == false){
-             this.result[this.currentIndex+1] = this.errorImg;
-           }
-           $('#'+this.moduleId+' img' ).attr('src', this.result[this.currentIndex+1] );
-           this.currentIndex++;
-           console.log('img : ' + this.result[this.currentIndex]);
-           this.sendDataToShinny();
-        }
-        
+      setCanvas(this.moduleId, this.result[0]);
+    }
+    if (this.moduleId === "spcs_idntfctn_id_rf_2") {
+      setCanvas(this.moduleId, this.result[0]);
+    }
+    if (this.moduleId === 'pttrn_rcgntn_orgnl_imgs_1') {
+      //console.log('case pttrn_rcgntn_orgnl_imgs_1');
+      setCanvas(this.moduleId, this.result[0]);
+    }
+    if (this.moduleId === 'pttrn_rcgntn_orgnl_imgs_2') {
+      //console.log('case pttrn_rcgntn_orgnl_imgs_2');
+      setCanvas(this.moduleId, this.result[0]);
     }
 
-    prev() {
-       //console.log('prev ' + this.moduleId + 'indx ' + this.currentIndex);
-       
-        if(this.currentIndex == 0){
-        }else{
-             $('#'+this.moduleId+' img' ).attr('src', this.result[this.currentIndex-1] );
-             this.currentIndex--;
-             //console.log('img : ' + this.result[this.currentIndex]);
-             this.sendDataToShinny();
-        }
-       
+    // PoaccherCam Images
+
+    if ((this.moduleId).includes("cmngmnt_id_rf")) {
+      console.log("case poachercams : " + this.moduleId);
+      console.log("image name : " + this.result[0]);
+      let src = (this.result[0]).substring((this.result[0]).lastIndexOf("/") + 1, (this.result[0]).length);
+      console.log("src : " + src);
+      setCanvas(this.moduleId, src);
     }
 
-    imgexist(image_url){
-    
-          let xmlhttp = new XMLHttpRequest();
-          xmlhttp.open("GET", image_url, false);
-          xmlhttp.send();
-          if (xmlhttp.status==200) {
-            return true;
-          }
-          return false;
-      
+    this.sendDataToShinny();
+
+  }
+
+  restart() {
+    this.result.length = 0;
+    this.currentIndex = 0;
+  }
+
+
+  readServerData(response) {
+
+    let respArray = [];
+    if (response === null) {
+      console.log(" Error in reading your images.Please check if all requirements are provided.");
+    } else {
+      //respArray = response.split(',');
+      //respArray.splice(0, 1);
+      console.log("response : " + response);
+      respArray = response.split("\n");
+      respArray.shift();
+
+
+      if (respArray[respArray.length - 1] == "") {
+        //console.log('pop');
+        respArray.pop();
+      }
+
+      for (let i = 0; i < respArray.length; i++) {
+        //respArray[i].replace(',','');
+        let src = respArray[i].substring(respArray[i].indexOf('/'), respArray[i].lastIndexOf('/')) + '/' + respArray[i].substring(0, respArray[i].indexOf('/'));
+        //src.replace(',','');
+        //console.log(src);
+        console.log(src.replace(',', ''));
+        this.result.push(src.replace(',', ''));
+      }
+      //console.log("respArray[0] : " + respArray[0]);
+      // respArray[0] = respArray[respArray.length - 1] + respArray[0];
+      // respArray.splice(respArray.length - 1, 1);
+      // this.result =  this.processImgSrc(respArray);
+      console.log(this.moduleId + 'Total Imgs : ' + (this.result.length));
+      //console.log(this.result)
+      this.displayImage();
+
     }
+  }
 
-    sendDataToShinny(){
-    
-      if(this.moduleId == "spcs_idntfctn_id_rf_1" || this.moduleId == "spcs_idntfctn_id_rf_2" ){
+  reset() {
+    console.log('reset ' + this.moduleId + 'indx ' + this.currentIndex);
 
-          let src = this.result[this.currentIndex];
-          let imgname = src.substring(src.lastIndexOf("/") + 1, src.length );
-    
-          if(this.moduleId == "spcs_idntfctn_id_rf_1"){
-            Shiny.setInputValue("spcs_idntfctn_id_rf_1_curr_img", imgname);
-          }
-          if(this.moduleId == "spcs_idntfctn_id_rf_2"){
-            Shiny.setInputValue("spcs_idntfctn_id_rf_2_curr_img", imgname);
-          }
-      }
-    }
+    this.currentIndex = 0;
+    console.log('img : ' + this.result[this.currentIndex]);
+    this.sendDataToShinny();
+    $('#' + this.moduleId + ' img').attr('src', this.result[this.currentIndex]);
 
-    resetHandlers(msg)
-    {
-      console.log('resetHandlers');
-      if(msg === 'noImages'){
-        Shiny.setInputValue('no_srv_imgs', null);
+  }
+
+  next() {
+    console.log('next : ' + this.moduleId + 'indx ' + this.currentIndex);
+
+
+    if (this.currentIndex == (this.result).length - 1) {} else {
+      if (this.imgexist(this.result[this.currentIndex + 1]) == false) {
+        this.result[this.currentIndex + 1] = this.errorImg;
       }
-      else{
-        Shiny.setInputValue('mssng_srv_imgs', null);
-      }
+      $('#' + this.moduleId + ' img').attr('src', this.result[this.currentIndex + 1]);
+      this.currentIndex++;
+      console.log('img : ' + this.result[this.currentIndex]);
+      this.sendDataToShinny();
     }
 
   }
+
+  prev() {
+    //console.log('prev ' + this.moduleId + 'indx ' + this.currentIndex);
+
+    if (this.currentIndex == 0) {} else {
+      $('#' + this.moduleId + ' img').attr('src', this.result[this.currentIndex - 1]);
+      this.currentIndex--;
+      //console.log('img : ' + this.result[this.currentIndex]);
+      this.sendDataToShinny();
+    }
+
+  }
+
+  imgexist(image_url) {
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", image_url, false);
+    xmlhttp.send();
+    if (xmlhttp.status == 200) {
+      return true;
+    }
+    return false;
+
+  }
+
+  sendDataToShinny() {
+
+    if (this.moduleId == "spcs_idntfctn_id_rf_1" || this.moduleId == "spcs_idntfctn_id_rf_2") {
+
+      let src = this.result[this.currentIndex];
+      let imgname = src.substring(src.lastIndexOf("/") + 1, src.length);
+
+      if (this.moduleId == "spcs_idntfctn_id_rf_1") {
+        Shiny.setInputValue("spcs_idntfctn_id_rf_1_curr_img", imgname);
+      }
+      if (this.moduleId == "spcs_idntfctn_id_rf_2") {
+        Shiny.setInputValue("spcs_idntfctn_id_rf_2_curr_img", imgname);
+      }
+    }
+  }
+
+  resetHandlers(msg) {
+    console.log('resetHandlers');
+    if (msg === 'noImages') {
+      Shiny.setInputValue('no_srv_imgs', null);
+    } else {
+      Shiny.setInputValue('mssng_srv_imgs', null);
+    }
+  }
+
+}
--- inst/htmlwidgets/sngl_img_viewer.js
+++ inst/htmlwidgets/sngl_img_viewer.js
@@ -1,148 +1,176 @@
 HTMLWidgets.widget({
 
-  name: 'sngl_img_viewer',
+        name: 'sngl_img_viewer',
 
-  type: 'output',
+        type: 'output',
 
-  factory: function(el, width, height) {
+        factory: function (el, width, height) {
 
-    return {
+                return {
 
-      renderValue: function(x) {
+                        renderValue: function (x) {
 
-        console.log("sngl_img_viewer.js 12-06-2020 16:23");
-        
-        let spcs_idntfctns_scndry_img = new viewerClass(
-                                            "spcs_idntfctn_id_rf_2",
-                                            "img_idntfctn_scndry.csv"),
-                spcs_idntfctns_prmry_img = new viewerClass(
-                                            "spcs_idntfctn_id_rf_1",
-                                            "img_idntfctn_prmry.csv"),
-                pttrn_rcgntn_vw_orgnls_1 = new viewerClass(
-                                        "pttrn_rcgntn_orgnl_imgs_1",
-                                        "pttrn_rcgntn_fltrd_rslts_prmry.csv"),
-                pttrn_rcgntn_vw_orgnls_2 = new viewerClass(
-                                        "pttrn_rcgntn_orgnl_imgs_2",
-                                        "pttrn_rcgntn_fltrd_rslts_scndry.csv");
+                                console.log("sngl_img_viewer.js 12-06-2020 16:23");
 
-        // Handle extract images buttons 
-        Shiny.addCustomMessageHandler("spcs_idntfctn_extrt_id_button_rf_1",
-                function(mesg) {
-                  spcs_idntfctn_extrt_id_button_rf_1
-                  spcs_idntfctns_prmry_img.restart();
-                  spcs_idntfctns_prmry_img.fetchServerData("img_idntfctn_prmry.csv");
-                  $('#spcs_idntfctn_id_rf_1').css({'outline': '0px solid transparent'});
-                  $('#spcs_idntfctn_id_rf_1').attr('tabindex', '0');
-                  $('#spcs_idntfctn_id_rf_1').focus();
+                                let spcs_idntfctns_scndry_img = new viewerClass(
+                                                "spcs_idntfctn_id_rf_2",
+                                                "img_idntfctn_scndry.csv"),
+                                        spcs_idntfctns_prmry_img = new viewerClass(
+                                                "spcs_idntfctn_id_rf_1",
+                                                "img_idntfctn_prmry.csv"),
+                                        pttrn_rcgntn_vw_orgnls_1 = new viewerClass(
+                                                "pttrn_rcgntn_orgnl_imgs_1",
+                                                "pttrn_rcgntn_fltrd_rslts_prmry.csv"),
+                                        pttrn_rcgntn_vw_orgnls_2 = new viewerClass(
+                                                "pttrn_rcgntn_orgnl_imgs_2",
+                                                "pttrn_rcgntn_fltrd_rslts_scndry.csv");
 
-                  $('#currnt-img_id_rf_1').click(function() {
-                        //console.log('clicked currnt-img_id_rf_1');
-                        $('#spcs_idntfctn_id_rf_1').focus();
-                });
-                }
-        );
 
-         Shiny.addCustomMessageHandler("spcs_idntfctn_extrt_id_button_rf_2",
-                function(mesg) {
-                  spcs_idntfctns_scndry_img.restart();
-                  spcs_idntfctns_scndry_img.fetchServerData("img_idntfctn_scndry.csv");
-                  $('#spcs_idntfctn_id_rf_2').css({'outline': '0px solid transparent'});
-                  $('#spcs_idntfctn_id_rf_2').attr('tabindex', '0');
-                  $('#spcs_idntfctn_id_rf_2').focus();
+                                // Poacher Cam Viewer
 
-                  $('#currnt-img_id_rf_2').click(function() {
-                        //console.log('clicked currnt-img_id_rf_2');
-                        $('#spcs_idntfctn_id_rf_2').focus();
-                 });
-                }
-        );
+                                Shiny.addCustomMessageHandler("cmngmnt_pchrcm_extrt_button",
+                                        function (mesg) {
 
-        Shiny.addCustomMessageHandler("spcs_idntfctn_reset_button_rf_1",
-                function(mesg) {
-                spcs_idntfctns_prmry_img.reset();
+                                                let num = parseInt(mesg);
+                                                console.log("num : " + num);
 
-                }
-        );
-        Shiny.addCustomMessageHandler("spcs_idntfctn_reset_button_rf_2",
-                function(mesg) {
-                spcs_idntfctns_scndry_img.reset();
-                }
-        );
+                                                for (let i = 1; i <= num; i++) {
+                                                        console.log("extracted " + i);
+                                                        let id = "cmngmnt_id_rf_" + i;
+                                                        let csvfile = "csv_file_" + i + ".csv";
+                                                        let obj = new viewerClass(id, csvfile);
+                                                        obj.restart();
+                                                        obj.fetchServerData(csvfile);
+                                                }
 
-        Shiny.addCustomMessageHandler("spcs_idntfctn_prev_button_rf_1",
-                function(mesg) {
-                spcs_idntfctns_prmry_img.prev();
-                }
-        );
+                                        }
+                                );
 
-        Shiny.addCustomMessageHandler("spcs_idntfctn_prev_button_rf_2",
-                function(mesg) {
-                spcs_idntfctns_scndry_img.prev();
-                }
-        );
+                                // Handle extract images buttons
+                                Shiny.addCustomMessageHandler("spcs_idntfctn_extrt_id_button_rf_1",
+                                        function (mesg) {
+                                                spcs_idntfctn_extrt_id_button_rf_1
+                                                spcs_idntfctns_prmry_img.restart();
+                                                spcs_idntfctns_prmry_img.fetchServerData("img_idntfctn_prmry.csv");
+                                                $('#spcs_idntfctn_id_rf_1').css({
+                                                        'outline': '0px solid transparent'
+                                                });
+                                                $('#spcs_idntfctn_id_rf_1').attr('tabindex', '0');
+                                                $('#spcs_idntfctn_id_rf_1').focus();
 
-        Shiny.addCustomMessageHandler("spcs_idntfctn_next_button_rf_1",
-                function(mesg) {
-                   spcs_idntfctns_prmry_img.next();
+                                                $('#currnt-img_id_rf_1').click(function () {
+                                                        //console.log('clicked currnt-img_id_rf_1');
+                                                        $('#spcs_idntfctn_id_rf_1').focus();
+                                                });
+                                        }
+                                );
 
-                }
-        );
-        Shiny.addCustomMessageHandler("spcs_idntfctn_next_button_rf_2",
-                function(mesg) {
-                   spcs_idntfctns_scndry_img.next();
-                }
-        );
+                                Shiny.addCustomMessageHandler("spcs_idntfctn_extrt_id_button_rf_2",
+                                        function (mesg) {
+                                                spcs_idntfctns_scndry_img.restart();
+                                                spcs_idntfctns_scndry_img.fetchServerData("img_idntfctn_scndry.csv");
+                                                $('#spcs_idntfctn_id_rf_2').css({
+                                                        'outline': '0px solid transparent'
+                                                });
+                                                $('#spcs_idntfctn_id_rf_2').attr('tabindex', '0');
+                                                $('#spcs_idntfctn_id_rf_2').focus();
 
-        // original imgs vwers
-        Shiny.addCustomMessageHandler("pttrn_rcgntn_orgnl_imgs_dsply_button",
-                function(mesg) {
-                        pttrn_rcgntn_vw_orgnls_1.restart();
-                        pttrn_rcgntn_vw_orgnls_1.fetchServerData("pttrn_rcgntn_fltrd_rslts_prmry.csv");
-                        $('#pttrn_rcgntn_orgnl_imgs_1').attr('tabindex', '0');
-                        $('#pttrn_rcgntn_orgnl_imgs_1').css({'outline': '0px solid transparent'});
-                        $('#pttrn_rcgntn_orgnl_imgs_1').focus();
-                        
-                        pttrn_rcgntn_vw_orgnls_2.restart();
-                        pttrn_rcgntn_vw_orgnls_2.fetchServerData("pttrn_rcgntn_fltrd_rslts_scndry.csv");
-                        $('#pttrn_rcgntn_orgnl_imgs_2').attr('tabindex', '0');
-                        $('#pttrn_rcgntn_orgnl_imgs_2').css({'outline': '0px solid transparent'});
-                        $('#pttrn_rcgntn_orgnl_imgs_2').focus();
+                                                $('#currnt-img_id_rf_2').click(function () {
+                                                        $('#spcs_idntfctn_id_rf_2').focus();
+                                                });
+                                        }
+                                );
 
-                        $('#currnt-img_orgnl_imgs_2').click(function() {
-                                //console.log('clicked pttrn_rcgntn_orgnl_imgs_2');
-                                $('#pttrn_rcgntn_orgnl_imgs_1').focus();
-                        });
-                        
-                        $('#currnt-img_orgnl_imgs_1').click(function() {
-                                //console.log('clicked pttrn_rcgntn_orgnl_imgs_1');
-                                $('#pttrn_rcgntn_orgnl_imgs_1').focus();
-                        });
-                }
-        );
+                                Shiny.addCustomMessageHandler("spcs_idntfctn_reset_button_rf_1",
+                                        function (mesg) {
+                                                spcs_idntfctns_prmry_img.reset();
 
-        Shiny.addCustomMessageHandler("pttrn_rcgntn_orgnl_imgs_prev_button",
-                function(mesg) {
-                        pttrn_rcgntn_vw_orgnls_1.prev();
-                        pttrn_rcgntn_vw_orgnls_2.prev();  
-                }
-        );
+                                        }
+                                );
+                                Shiny.addCustomMessageHandler("spcs_idntfctn_reset_button_rf_2",
+                                        function (mesg) {
+                                                spcs_idntfctns_scndry_img.reset();
+                                        }
+                                );
 
-        Shiny.addCustomMessageHandler("pttrn_rcgntn_orgnl_imgs_reset_button",
-                function(mesg) {
-                        pttrn_rcgntn_vw_orgnls_1.reset();
-                        pttrn_rcgntn_vw_orgnls_2.reset();
-                }
-        );
+                                Shiny.addCustomMessageHandler("spcs_idntfctn_prev_button_rf_1",
+                                        function (mesg) {
+                                                spcs_idntfctns_prmry_img.prev();
+                                        }
+                                );
 
-        Shiny.addCustomMessageHandler("pttrn_rcgntn_orgnl_imgs_next_buttonn",
-                function(mesg) {
-                        pttrn_rcgntn_vw_orgnls_1.next();
-                        pttrn_rcgntn_vw_orgnls_2.next();
-                }
-        );
-      },
+                                Shiny.addCustomMessageHandler("spcs_idntfctn_prev_button_rf_2",
+                                        function (mesg) {
+                                                spcs_idntfctns_scndry_img.prev();
+                                        }
+                                );
 
-      resize: function(width, height) {}
-    };
-  }
+                                Shiny.addCustomMessageHandler("spcs_idntfctn_next_button_rf_1",
+                                        function (mesg) {
+                                                spcs_idntfctns_prmry_img.next();
+
+                                        }
+                                );
+                                Shiny.addCustomMessageHandler("spcs_idntfctn_next_button_rf_2",
+                                        function (mesg) {
+                                                spcs_idntfctns_scndry_img.next();
+                                        }
+                                );
+
+                                // original imgs vwers
+                                Shiny.addCustomMessageHandler("pttrn_rcgntn_orgnl_imgs_dsply_button",
+                                        function (mesg) {
+                                                pttrn_rcgntn_vw_orgnls_1.restart();
+                                                pttrn_rcgntn_vw_orgnls_1.fetchServerData("pttrn_rcgntn_fltrd_rslts_prmry.csv");
+                                                $('#pttrn_rcgntn_orgnl_imgs_1').attr('tabindex', '0');
+                                                $('#pttrn_rcgntn_orgnl_imgs_1').css({
+                                                        'outline': '0px solid transparent'
+                                                });
+                                                $('#pttrn_rcgntn_orgnl_imgs_1').focus();
+
+                                                pttrn_rcgntn_vw_orgnls_2.restart();
+                                                pttrn_rcgntn_vw_orgnls_2.fetchServerData("pttrn_rcgntn_fltrd_rslts_scndry.csv");
+                                                $('#pttrn_rcgntn_orgnl_imgs_2').attr('tabindex', '0');
+                                                $('#pttrn_rcgntn_orgnl_imgs_2').css({
+                                                        'outline': '0px solid transparent'
+                                                });
+                                                $('#pttrn_rcgntn_orgnl_imgs_2').focus();
+
+                                                $('#currnt-img_orgnl_imgs_2').click(function () {
+                                                        //console.log('clicked pttrn_rcgntn_orgnl_imgs_2');
+                                                        $('#pttrn_rcgntn_orgnl_imgs_1').focus();
+                                                });
+
+                                                $('#currnt-img_orgnl_imgs_1').click(function () {
+                                                        //console.log('clicked pttrn_rcgntn_orgnl_imgs_1');
+                                                        $('#pttrn_rcgntn_orgnl_imgs_1').focus();
+                                                });
+                                        }
+                                );
+
+                                Shiny.addCustomMessageHandler("pttrn_rcgntn_orgnl_imgs_prev_button",
+                                        function (mesg) {
+                                                pttrn_rcgntn_vw_orgnls_1.prev();
+                                                pttrn_rcgntn_vw_orgnls_2.prev();
+                                        }
+                                );
+
+                                Shiny.addCustomMessageHandler("pttrn_rcgntn_orgnl_imgs_reset_button",
+                                        function (mesg) {
+                                                pttrn_rcgntn_vw_orgnls_1.reset();
+                                                pttrn_rcgntn_vw_orgnls_2.reset();
+                                        }
+                                );
+
+                                Shiny.addCustomMessageHandler("pttrn_rcgntn_orgnl_imgs_next_buttonn",
+                                        function (mesg) {
+                                                pttrn_rcgntn_vw_orgnls_1.next();
+                                                pttrn_rcgntn_vw_orgnls_2.next();
+                                        }
+                                );
+                        },
+
+                        resize: function (width, height) {}
+                };
+        }
 });
